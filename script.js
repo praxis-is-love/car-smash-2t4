c72e6ddec9cefd4ccab9f855e300a482a3f3dca3
harry7557558 <harry7557558@gmail.com>
2024-01-11 15:00:31 -0500

    initial commit - or what?

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+    document.body.innerHTML += decodeURIComponent(escape(atob([
+        "PGkgc3R5bGU9ImNvbG9yO",
+        "MxMTE",
+        "PgpPaCBQcmF4aXMsIG15IGRlYXIsPGJyLz4KaG93IEkgY2hlcmlzaCB0aGUgZGF5cyB3ZSBzcGVudCB0b2dldGhlc",
+        "w8YnIvPgppb",
+        "BlbmRsZXNzIHdyaXRpbmcgYW5kIGRlc2lnb",
+        "Bwcm9qZWN0cy48YnIvPgpXaG8ga25ldyBFbmdTY2kgY291bGQgYmUgc28gbXVjaCBmdW4/PGJyLz4K4o",
+        "SIPCdkqvwnZOH8J2StvCdk43wnZK+8J2T",
+        "CD",
+        "hLDwnZOD8J2Sv+KEtPCdk47",
+        "hK/wnZOHICZsdDszICZsdDszICZsdDszCjwvaT4="
+    ].join('i'))));
+};
